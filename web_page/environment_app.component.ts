8e60afecf5417d781e0eeaed4822d9d1a42c567c
David Téllez Rodríguez <davtelrod@gmail.com>
2017-02-06 22:41:43 +0100

    master Version v0.0.1 running

---
+++ web_page/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'pwp-title'
+})
+
+@View({
+  template: '<h2>David\'s Personal Web Page</h2>'
+})
+
+export class AppComponent {
+
+}
